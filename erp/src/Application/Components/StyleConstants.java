30915f5df0cbd30bcac818805cf5fd2092435354
Mohd Zaid <mdzaid0205@gmail.com>
2025-11-25 17:38:37 +0530

    added styles

---
+++ erp/src/Application/Components/StyleConstants.java
@@ -0,0 +1,25 @@
+package Application.Components;
+
+import java.awt.Color;
+import java.awt.Font;
+
+public class StyleConstants {
+    public static final Color PRIMARY_COLOR = new Color(100, 100, 255);
+    public static final Color SECONDARY_COLOR = new Color(75, 75, 200);
+    public static final Color TERTIARY_COLOR = new Color(50, 50, 150);
+    public static final Color ACCENT_COLOR  = new Color(100, 100, 255);
+    public static final Color BG_COLOR      = new Color(255, 255, 255);
+    public static final Color TEXT_COLOR    = new Color( 55,  55,  55);
+    public static final Color DISABLED_COLOR= new Color(150, 150, 160);
+
+    public static final Color GREEN = new Color(100, 255, 100);
+    public static final Color RED = new Color(255, 100, 100);
+
+    public static final Color WHITE         = Color.WHITE;
+    public static final Color DIM_WHITE     = new Color(225, 225, 225);
+    public static final Color GRAY          = Color.GRAY;
+
+    public static final Font HEADER_FONT = new Font("Segoe UI", Font.BOLD, 24);
+    public static final Font BUTTON_FONT = new Font("Segoe UI", Font.BOLD, 14);
+    public static final Font NORMAL_FONT = new Font("Segoe UI", Font.PLAIN, 14);
+}
